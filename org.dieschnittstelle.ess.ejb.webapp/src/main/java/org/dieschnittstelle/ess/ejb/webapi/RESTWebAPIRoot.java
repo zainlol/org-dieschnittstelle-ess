f7650e5ed52e1b848ae0a907f44d2a427b7a38b1
jk <joern.kreutel@beuth-hochschule.de>
2020-03-12 18:31:22 +0100

    remove unnecessary dependencies from pom after finally fixing resteasy/jackson issue by setting -Dresteasy.preferJacksonOverJsonB=true

--- org.dieschnittstelle.ess.ejb.webapp/src/main/java/org/dieschnittstelle/ess/ejb/webapi/RESTWebAPIRoot.java
+++ org.dieschnittstelle.ess.ejb.webapp/src/main/java/org/dieschnittstelle/ess/ejb/webapi/RESTWebAPIRoot.java
@@ -5,6 +5,11 @@ import javax.ws.rs.core.Application;
 
 import org.apache.logging.log4j.Logger;
 
+/*
+ * Note that in order for the webapi to work correctly, the option
+ * -Dresteasy.preferJacksonOverJsonB=true must be set when starting
+ * jboss, otherwise JsonTypeInfo will not be considered, see https://docs.jboss.org/resteasy/docs/4.3.1.Final/userguide/html/JAX-RS_2.1_additions.html
+ */
 @ApplicationPath("/api")
 public class RESTWebAPIRoot extends Application {
 
